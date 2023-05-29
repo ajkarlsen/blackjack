fb3009422b70b2d58cefd74693b79696fd7b64fc
Alex <alex.j.karlsen@gmail.com>
2023-05-29 10:42:45 +0200

    Working blackjack

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let playerCardEl = document.getElementById("player-card-el")
 let dealerCardEl = document.getElementById("dealer-card-el")
 let playerSumEl = document.getElementById("player-sum-el")
 let dealerSumEl = document.getElementById("dealer-sum-el")
+let messageEl = document.getElementById("message-el")
 
 let player = {
     name: "Alex",
@@ -24,7 +25,11 @@ function newGame() {
         dealerCards.push(getRandomCard())
         playerSum = playerCards[0]+playerCards[1]
         dealerSum = dealerCards[0]
-        renderGame()
+        if (playerSum === 21) {
+            blackJack()
+        } else {
+            renderGame()
+        }
     }
 }
 
@@ -33,20 +38,57 @@ function renderGame() {
     playerSumEl.textContent = "Sum: " + playerSum
     dealerSumEl.textContent = "Sum: " + dealerSum
 
+    playerCardEl.textContent = "Cards: "
+    dealerCardEl.textContent = "Cards: "
     for (let i = 0; i < playerCards.length; i++) {
         playerCardEl.textContent += " " + playerCards[i]
     }
     for (let i = 0; i < dealerCards.length; i++) {
         dealerCardEl.textContent += " " + dealerCards[i]
     }
+    if (playerSum > 21) {
+        isAlive = false
+        messageEl.textContent = "You're out, play again?"
+    } else if (playerSum === 21) {
+        messageEl.textContent = "Stand to see dealer"
+        hasBlackJack = true
+    } else {
+        messageEl.textContent = "New card?"
+    }
 }
 
 function newCard() {
-    let newCard = getRandomCard()
-    playerCards.push(newCard)
-    playerSum += newCard
+    if (isAlive === true && hasBlackJack === false) {
+        let newCard = getRandomCard()
+        playerCards.push(newCard)
+        playerSum += newCard
+        renderGame()
+    }
 }
 
+function dealerTurn() {
+    if (isAlive === true) {
+        while (dealerSum < playerSum && dealerSum < 17) {
+            let newCard = getRandomCard()
+            dealerCards.push(newCard)
+            dealerSum += newCard
+            renderGame()
+        }
+        if (dealerSum > 21) {
+            messageEl.textContent = "Dealer busts, you won"
+            isAlive = false
+        } else if (dealerSum > playerSum) {
+            messageEl.textContent = "Dealer wins"
+            isAlive = false
+        } else if (dealerSum === playerSum) {
+            isAlive = false
+            messageEl.textContent = "Stand off, money back"
+        } else {
+            isAlive = false
+            messageEl.textContent = "You win"
+        }
+    }
+}
 
 function getRandomCard() {
     let randomNumber = Math.ceil(Math.random() * 13)
