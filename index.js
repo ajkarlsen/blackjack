0fd65aee29bc71e2b96e752341f01e1a0cbaf3f9
Alex <alex.j.karlsen@gmail.com>
2023-05-29 11:44:01 +0200

    Ace function and blackjack function

--- index.js
+++ index.js
@@ -18,18 +18,17 @@ let player = {
 function newGame() {
     if (isAlive === false || hasBlackJack === true) {
         isAlive = true
-        hasBlackJack = false
         playerCards = []
         dealerCards = []
-        playerCards.push(getRandomCard(), getRandomCard())
-        dealerCards.push(getRandomCard())
-        playerSum = playerCards[0]+playerCards[1]
+        card1 = getRandomCard(0)
+        playerSum = card1
+        card2 = getRandomCard(playerSum)
+        playerSum += card2
+        playerCards.push(card1, card2)
+        dealerCards.push(getRandomCard(0))
         dealerSum = dealerCards[0]
-        if (playerSum === 21) {
-            blackJack()
-        } else {
-            renderGame()
-        }
+
+         renderGame()
     }
 }
 
@@ -49,9 +48,12 @@ function renderGame() {
     if (playerSum > 21) {
         isAlive = false
         messageEl.textContent = "You're out, play again?"
+    } else if (playerSum === 21 && playerCards.length === 2) {
+        isAlive = false
+        messageEl.textContent = "Blackjack, 1.5 times your money back"
     } else if (playerSum === 21) {
-        messageEl.textContent = "Stand to see dealer"
-        hasBlackJack = true
+        messageEl.textContent = "Dealer playing"
+        dealerTurn()
     } else {
         messageEl.textContent = "New card?"
     }
@@ -59,41 +61,58 @@ function renderGame() {
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
-        let newCard = getRandomCard()
+        let newCard = getRandomCard(playerSum)
         playerCards.push(newCard)
         playerSum += newCard
+        if (playerSum > 21) {
+            for (let i = 0; i < playerCards.length; i++) {
+                if (playerCards[i] === 11) {
+                    playerCards[i] = 1
+                    playerSum -= 10
+                }
+            }
+        }
         renderGame()
     }
 }
 
-function dealerTurn() {
+async function dealerTurn() {
     if (isAlive === true) {
+        isAlive = false
         while (dealerSum < playerSum && dealerSum < 17) {
-            let newCard = getRandomCard()
+            await wait(1000);
+            let newCard = getRandomCard(dealerSum)
             dealerCards.push(newCard)
             dealerSum += newCard
+            if (dealerSum > 21) {
+                for (let i = 0; i < dealerCards.length; i++) {
+                    if (dealerCards[i] === 11) {
+                        dealerCards[i] = 1
+                        dealerSum -= 10
+                    }
+                }
+            }
             renderGame()
         }
         if (dealerSum > 21) {
             messageEl.textContent = "Dealer busts, you won"
-            isAlive = false
         } else if (dealerSum > playerSum) {
             messageEl.textContent = "Dealer wins"
-            isAlive = false
         } else if (dealerSum === playerSum) {
-            isAlive = false
             messageEl.textContent = "Stand off, money back"
         } else {
-            isAlive = false
             messageEl.textContent = "You win"
         }
     }
 }
 
-function getRandomCard() {
+function getRandomCard(sum) {
     let randomNumber = Math.ceil(Math.random() * 13)
-    if (randomNumber === 1) {
+    console.log(playerSum + "," + dealerSum)
+    if (randomNumber === 1 && sum + 11 <= 21) {
         return 11
+    } else if (randomNumber === 1 && sum + 11 > 21) {
+        return 1
     } else if (randomNumber >= 10) {
         return 10
     } else {
@@ -101,3 +120,8 @@ function getRandomCard() {
     }
 }
 
+function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(()=> {resolve('')}, ms);
+    })
+}
